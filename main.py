f87a3cbd9b20b0491e09d9278a287a5458b1686a
Ilya Mikhasik <imikhassik@gmail.com>
2025-02-09 12:48:36 +0200

    Add canvas

--- main.py
+++ main.py
@@ -3,38 +3,11 @@ from tkinter import ttk
 
 root = Tk()
 
-content = ttk.Frame(root, padding=(3,3,12,12))
-frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
-namelbl = ttk.Label(content, text="Name")
-name = ttk.Entry(content)
+canvas = Canvas(root, width=1500, height=750, background='white')
 
-onevar = BooleanVar(value=True)
-twovar = BooleanVar(value=False)
-threevar = BooleanVar(value=True)
-
-one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
-two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
-three = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
-ok = ttk.Button(content, text="Okay")
-cancel = ttk.Button(content, text="Cancel")
-
-content.grid(column=0, row=0, sticky=(N, S, E, W))
-frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
-namelbl.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
-name.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), padx=5, pady=5)
-one.grid(column=0, row=3)
-two.grid(column=1, row=3)
-three.grid(column=2, row=3)
-ok.grid(column=3, row=3)
-cancel.grid(column=4, row=3)
+canvas.grid(column=0, row=0, sticky="nswe")
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-content.columnconfigure(0, weight=3)
-content.columnconfigure(1, weight=3)
-content.columnconfigure(2, weight=3)
-content.columnconfigure(3, weight=1)
-content.columnconfigure(4, weight=1)
-content.rowconfigure(1, weight=1)
 
 root.mainloop()
