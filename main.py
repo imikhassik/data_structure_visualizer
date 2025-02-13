070018ec51a180b6b8ed86c0dfdd4738003d04d9
Ilya Mikhasik <imikhassik@gmail.com>
2025-02-13 13:10:59 +0200

    Rename and reorder methods

--- main.py
+++ main.py
@@ -7,26 +7,35 @@ class MainWindow:
         self._root = Tk()
         self._example_value = StringVar()
         self._example_value.trace_add("write", self._parse_user_entry)
-        self._configure()
-        self._grid()
+        self._configure_window()
+        self._create_widgets()
+        self._configure_widgets()
+        self._grid_widgets()
 
-    def _configure(self):
+    def _configure_window(self):
         self._root.columnconfigure(0, weight=1)
         self._root.rowconfigure(2, weight=1)
 
-    def _populate(self):
-        self._content = ttk.Frame(self._root, padding=12)
-        self._example_label = ttk.Label(self._content, text="Example:")
-        self._steps_label = ttk.Label(self._content, text="Steps:")
-        self._example_entry = ttk.Entry(self._content, textvariable=self._example_value)
+    def _create_widgets(self):
+        self._content = ttk.Frame(self._root)
+        self._example_label = ttk.Label(self._content)
+        self._steps_label = ttk.Label(self._content)
+        self._example_entry = ttk.Entry(self._content)
 
-    def _grid(self):
-        self._populate()
+    def _configure_widgets(self):
+        self._content.configure(padding=12)
+        self._example_label.configure(text="Example:")
+        self._steps_label.configure(text="Steps:")
+        self._example_entry.configure(textvariable=self._example_value)
+
+    def _grid_widgets(self):
         self._content.grid(column=0, row=0, sticky="nsew")
         self._example_label.grid(column=0, row=0, sticky="w")
         self._steps_label.grid(column=2, row=0, sticky="w")
         self._example_entry.grid(column=0, row=1, columnspan=2, sticky="we")
 
+    #TODO: test everything i have so far
+    #TODO: add error handling for user input
     def _parse_user_entry(self, *args):
         print(self._example_value.get().split(','))
 
