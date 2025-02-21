d4a3cb65501a3be27802d0c672fff6fe994d55de
Ilya Mikhasik <imikhassik@gmail.com>
2025-02-21 09:09:15 +0200

    Experiments with regex

--- entry_parser.py
+++ entry_parser.py
@@ -1,3 +1,5 @@
+import re
+
 # TODO: test
 # TODO: add error handling for user input
 class EntryParser:
@@ -6,9 +8,12 @@ class EntryParser:
         self.index = 0
 
     def parse(self, entry_value):
-        for c in entry_value:
-            if c == "[":
-                self.result.append([])
-                self.index = len(self.result) - 1
-            elif c.isalnum():
-                self.result[self.index].append(c)
+        p = re.compile(r'\w')
+        self.result = re.findall(p, entry_value)
+        print(self.result)
+        # for c in entry_value:
+        #     if c == "[":
+        #         self.result.append([])
+        #         self.index = len(self.result) - 1
+        #     elif c.isalnum():
+        #         self.result[self.index].append(c)
