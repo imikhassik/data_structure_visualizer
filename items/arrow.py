99d5d24e7e445c25bdaf454f4e753d4605c539cb
Ilya Mikhasik <imikhassik@gmail.com>
2025-02-27 07:39:54 +0200

    Update to arrow head drag and drop

--- items/arrow.py
+++ items/arrow.py
@@ -12,8 +12,7 @@ class Arrow:
         self.head_x = node.coordinates.x + NODE_DIAMETER_IN_PIXELS + ARROW_LENGTH_IN_PIXELS
         self.head_y = node.coordinates.y + NODE_DIAMETER_IN_PIXELS // 2
 
-        self.body = None
-        self.point = None
+        self.arrow = None
         self.create_arrow()
 
         self.dragging = False
@@ -21,7 +20,7 @@ class Arrow:
 
 
     def create_arrow(self):
-        self.body = self.canvas.create_line(
+        self.arrow = self.canvas.create_line(
             self.tail_x, self.tail_y, self.head_x, self.head_y,
             arrow='last',
             arrowshape=ARROW_SHAPE,
@@ -30,17 +29,9 @@ class Arrow:
             activefill='#00FF00'
         )
 
-        self.point = self.canvas.create_oval(
-            self.head_x - 5,
-            self.head_y - 5,
-            self.head_x + 5,
-            self.head_y + 5,
-            tags=(Tag.ALL_CANVAS_ITEMS, Tag.ARROW_POINT)
-        )
-
-        self.canvas.tag_bind(self.point, "<Button-1>", self._start_drag)
-        self.canvas.tag_bind(self.point, "<B1-Motion>", self._drag)
-        self.canvas.tag_bind(self.point, "<ButtonRelease-1>", self._stop_drag)
+        self.canvas.tag_bind(self.arrow, "<Button-1>", self._start_drag)
+        self.canvas.tag_bind(self.arrow, "<B1-Motion>", self._drag)
+        self.canvas.tag_bind(self.arrow, "<ButtonRelease-1>", self._stop_drag)
 
     def _start_drag(self, event):
         self.dragging = True
@@ -50,11 +41,9 @@ class Arrow:
         if self.dragging:
             dx = event.x - self.last_x
             dy = event.y - self.last_y
-            self.canvas.move(self.point, dx, dy)
             self.head_x += dx
             self.head_y += dy
-            self.canvas.coords(self.body, self.tail_x, self.tail_y, self.head_x, self.head_y)
-            self.canvas.coords(self.point, self.head_x - 5, self.head_y - 5, self.head_x + 5, self.head_y + 5)
+            self.canvas.coords(self.arrow, self.tail_x, self.tail_y, self.head_x, self.head_y)
             self.last_x, self.last_y = event.x, event.y
 
     def _stop_drag(self, event):
